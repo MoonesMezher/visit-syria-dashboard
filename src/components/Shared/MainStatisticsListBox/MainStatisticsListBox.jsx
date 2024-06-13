5a55fab725db44803147b4af71a09fa3c7fbe9eb
Ayham Ibrahim <ayhamfacebook1999@gmail.com>
2024-06-13 03:39:13 +0300

    add list and user statistics

--- src/components/Shared/MainStatisticsListBox/MainStatisticsListBox.jsx
+++ src/components/Shared/MainStatisticsListBox/MainStatisticsListBox.jsx
@@ -1,6 +1,34 @@
-const MainStatisticsListBox = () => {
+import "./MainStatisticsListBox.css";
+const MainStatisticsListBox = ({title,icon,fixed}) => {
     return (
-        <div>MainStatisticsListBox</div>
+        <div className="card main-statistic-ay" style={{ height: "159px" }}>
+            <div className="card-header">
+                <h6 className="title">{title}</h6>
+                <span className="icon">{icon}</span>
+            </div>
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item">
+                    <div className="user-name">ayham</div>
+                    <div className="number">20 <span className="fixed-ay">{fixed}</span></div>
+                </li>
+                <li className="list-group-item">
+                    <div className="user-name">ayham</div>
+                    <div className="number">20 <span className="fixed-ay">{fixed}</span></div>
+                </li>
+                <li className="list-group-item">
+                    <div className="user-name">ayham</div>
+                    <div className="number">20 <span className="fixed-ay">{fixed}</span></div>
+                </li>
+                <li className="list-group-item">
+                    <div className="user-name">ayham</div>
+                    <div className="number">20 <span className="fixed-ay">{fixed}</span></div>
+                </li>
+                <li className="list-group-item">
+                    <div className="user-name">ayham</div>
+                    <div className="number">20 <span className="fixed-ay">{fixed}</span></div>
+                </li>
+            </ul>
+        </div>
     )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import MainButton from "../../components/Shared/MainButton/MainButton"
 import MainChart from "../../components/Shared/MainChart/MainChart"
 import MainStatisticsInfoBox from "../../components/Shared/MainStatisticsInfoBox/MainStatisticsInfoBox"
+import MainStatisticsListBox from "../../components/Shared/MainStatisticsListBox/MainStatisticsListBox"
 import MainTable from "../../components/Shared/MainTable/MainTable"
 import { dataPage1, headers } from "../../constant/staticData"
-
+import { MdOutlineArticle } from "react-icons/md";
+import { TbCoin } from "react-icons/tb";
 const Home = () => {
     return (
         <section>
@@ -12,6 +14,8 @@ const Home = () => {
             <MainButton text={'Add'}/>
             <MainStatisticsInfoBox title={'sss'} number={40} unit={'Km'}/>
             <MainStatisticsInfoBox title={'sss'} number={40} unit={'Km'}/>
+            <MainStatisticsListBox title="المستخدم صاحب اكبر عدد حجوزات" icon={<MdOutlineArticle/>} fixed="حجز"/>
+            <MainStatisticsListBox title="المستخدم الذي دفع اكبر مبلغ" icon={<TbCoin />} fixed={<TbCoin />}/>
         </section>
     )
 }
