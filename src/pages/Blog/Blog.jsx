09747eb2c3d0f238af6467ea364c3320956c058b
EsmaelAbd <esmaelabd51@gmail.com>
2024-06-24 14:44:40 +0300

    added blog-SatisticsBlog and changed on mainPhotoInput and mainButton

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,7 +1,86 @@
-const Blog = () => {
-    return (
-        <section>Blog</section>
-    )
-}
+import { MdAssistantPhoto } from "react-icons/md";
+import MainButton from "../../components/Shared/MainButton/MainButton";
+import MainSelect from "../../components/Shared/MainSelect/MainSelect";
+import MainTable from "../../components/Shared/MainTable/MainTable";
+import "./Blog.css";
+import { dataPage1, headers } from "./BlogStaticData";
+import MainPhotoInput from "../../components/Shared/MainPhotoInput/MainPhotoInput";
+import { useState } from "react";
+import MainPhotoGroupInput from "../../components/Shared/MainPhotoGroupInput/MainPhotoGroupInput";
+import MainInput from "../../components/Shared/MainInput/MainInput";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-export default Blog
+const Blog = () => {
+  const [mainImageFile, setMainImageFile] = useState(null);
+  const [mainImagePreview, setMainImagePreview] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [imgs, setImgs] = useState([]);
+
+  const sendData = (event) => {
+    event.preventDefault();
+    let formData = new FormData();
+    formData.append("title", title);
+    formData.append("content", content);
+    if (mainImageFile) {
+      formData.append("main_image", mainImageFile);
+    }
+    console.log(formData);
+    axios
+      .post("http://127.0.0.1:8000/api/add", formData)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const category = ["الطبيعة", "الأثرية"];
+  const options = ["عنوان المقالة", "تاريخ النشر", "id"];
+
+  return (
+    <section>
+      <div className="blog-header">
+        <MainSelect title="ترتيب حسب" options={options} />
+        <MainButton text={"اضافة مقال"} />
+      </div>
+      <div className="blog-table">
+        <MainTable data={dataPage1} headers={headers} />
+      </div>
+      <div className="blog-body">
+        <div className="blog-body-left">
+          <div className="img-and-label">
+            <p>صور اضافية</p>
+            <MainPhotoGroupInput imgs={imgs} setImgs={setImgs} />
+          </div>
+          <MainButton text={"نشر المقالة"} onClick={sendData} />
+        </div>
+        <div className="blog-body-right">
+          <div className="img-and-label">
+            <p>الصورة الرئيسية</p>
+            <div className="img-category">
+              <MainSelect title={"Category"} options={category} />
+              <MainPhotoInput
+                img={mainImagePreview}
+                setImgFile={setMainImageFile}
+                setImgPreview={setMainImagePreview}
+              />
+            </div>
+          </div>
+          <MainInput
+            label={"عنوان المقالة"}
+            type={"text"}
+            setInputValue={setTitle}
+          />
+          <MainInput
+            label={"محتوى المقالة"}
+            type={"textarea"}
+            setInputValue={setContent}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Blog;
--- src/pages/SatisticsBlog/SatisticsBlog.jsx
+++ src/pages/SatisticsBlog/SatisticsBlog.jsx
@@ -1,7 +1,97 @@
-const SatisticsBlog = () => {
-    return (
-        <section>SatisticsBlog</section>
-    )
-}
+import { FaUserGroup } from "react-icons/fa6";
+import MainChart from "../../components/Shared/MainChart/MainChart";
+import MainSelect from "../../components/Shared/MainSelect/MainSelect";
+import MainStatisticsInfoBox from "../../components/Shared/MainStatisticsInfoBox/MainStatisticsInfoBox";
+import MainStatisticsListBox from "../../components/Shared/MainStatisticsListBox/MainStatisticsListBox";
+import "./SatisticsBlog.css";
+import { IoMdStarOutline } from "react-icons/io";
 
-export default SatisticsBlog
+const SatisticsBlog = () => {
+  const options = ["خلال 6 اشهر", "خلال سنة"];
+  const list1 = [
+    {
+      name: "مدرج بصرى",
+      number: 15.265,
+    },
+    {
+      name: "..مقالة",
+      number: 10.568,
+    },
+    {
+      name: "..مقالة",
+      number: 9.524,
+    },
+    {
+      name: "..مقالة",
+      number: 8.544,
+    },
+    {
+      name: "..مقالة",
+      number: 8.125,
+    },
+  ];
+
+  const list2 = [
+    {
+      name: "التكية السليمانية",
+      number: 4.95,
+    },
+    {
+      name: "مقالة",
+      number: 4.92,
+    },
+    {
+      name: "مقالة",
+      number: 4.8,
+    },
+    {
+      name: "مقالة",
+      number: 4.78,
+    },
+    {
+      name: "مقالة",
+      number: 4.92,
+    },
+  ];
+  return (
+    <section>
+      <div className="blog-stat-header">
+        <MainSelect title={"خلال شهر"} option={options} />
+      </div>
+      <div className="blog-stat-body">
+        <div className="blog-stat-body-left">
+          <div className="blog-stat-info">
+            <MainStatisticsInfoBox
+              unit={"زيارة"}
+              number={"117K"}
+              title={"عدد زيارات المدونة"}
+            />
+            <MainStatisticsInfoBox
+              unit={"مقالة"}
+              number={"106k"}
+              title={"عدد مقالات المدونة"}
+            />
+          </div>
+
+          <MainChart title={"مخطط زيارة المدونة"} />
+        </div>
+        <div className="blog-stat-body-right">
+          <MainStatisticsListBox
+            title={"المقالات الاكثر زيارة"}
+            icon={<FaUserGroup />}
+            list={list1}
+            fixed={<FaUserGroup />}
+          />
+          <MainStatisticsListBox
+            title={"المقالات الاعلى تقييما"}
+            icon={<IoMdStarOutline />}
+            list={list2}
+            fixed={<IoMdStarOutline />}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SatisticsBlog;
